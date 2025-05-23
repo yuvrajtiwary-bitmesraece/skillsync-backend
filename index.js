39088476b631679448c6d7883ff8705baa88dbf4
Yuvraj Tiwary <156575870+yuvrajtiwary-bitmesraece@users.noreply.github.com>
2025-05-23 14:26:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-// 👇 Auth Route Include
+// Auth routes
 const authRoutes = require('./routes/auth');
 app.use('/api/auth', authRoutes);
 
